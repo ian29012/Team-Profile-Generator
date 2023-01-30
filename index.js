49d5d9afb2acc16836e9e837932e398b64e51c46
ian29012 <iancheng29012@gmail.com>
2023-01-30 10:11:16 +0000

    created the question list

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const employee = require('./lib/employee');
+
+Start()
+
+{
+    type: 'list',
+    choices: ['Engineer', 'Intern', 'Manager'],
+    message: 'Which type of team member would you like to add',
+    name: 'type',
+}
