2b7fa61052ce5fc872d931c5782dd4064dbde577
ian29012 <iancheng29012@gmail.com>
2023-01-30 23:01:36 +0000

    create the array to store the information of the prompt

--- index.js
+++ index.js
@@ -5,23 +5,78 @@ const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern.js');
 
-const roleQues = require('./lib/question.js');
+const roleQues = require('./lib/roleQues.js');
 const managerQues = require('./lib/ManagerQues.js');
 const engineerQues = require('./lib/EngineerQues');
 const internQues = require('./lib/InternQues');
 
+const employee = []
+
 // // function to initialize program
 function init() {
-    inquirer.prompt(internQues)
+    inquirer.prompt(managerQues)
     .then((data) => {
-        if (!data.confirm){
-        data.employeeId = new Intern(data);
-        console.log(data.employeeId)
-        console.log(data.employeeId.getSchool())
-        console.log(data.employeeId.getRole())
-        } else {
-            return init()
+        data.employeeId = new Manager(data);
+        employee.push(data.employeeId);
+        console.log(data.employeeId);
+        console.log("-------------")
+        console.log(employee)
+
+        if (data.confirm){
+            role()
+            console.log(employee)
         }
     });
 }
+
+function engineer() {
+    inquirer.prompt(engineerQues)
+    .then((data) => {
+        data.employeeId = new Engineer(data);
+        employee.push(data.employeeId);
+        console.log(data.employeeId)
+        console.log("-------------")
+        console.log(employee)
+
+        if (data.confirm){
+            role()
+            console.log(employee)
+        }
+    });
+}
+
+function intern() {
+    inquirer.prompt(internQues)
+    .then((data) => {
+        data.employeeId = new Intern(data);
+        employee.push(data.employeeId);
+        console.log(data.employeeId)
+        console.log("-------------")
+        console.log(employee)
+
+        if (data.confirm){
+            role()
+            console.log(employee)
+        }
+    });
+}
+
+function role(){
+    inquirer.prompt(roleQues)
+    .then((response) => {
+        switch (response.employeeRole) {
+            case 'Manager' :
+            init();
+            break;
+            case 'Engineer' :
+            engineer();
+            break;
+            case 'Intern' :
+            intern();
+            break;
+            }
+    })
+}
+
+
 init()
