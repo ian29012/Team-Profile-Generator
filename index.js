6dea3744f7a6f113746595335cd8cde4efe3f290
ian29012 <iancheng29012@gmail.com>
2023-01-30 22:28:26 +0000

    fixed the path of the question and the splited class

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager.js');
-const Engineer = require('./lib/generator.js');
-const Intern = require('./lib/generator.js');
-const Employee = require('./lib/generator.js');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern.js');
+
 const roleQues = require('./lib/question.js');
-const managerQues = require('./lib/managerQues.js');
-const engineerQues = require('./lib/question.js');
-const internQues = require('./lib/question.js');
+const managerQues = require('./lib/ManagerQues.js');
+const engineerQues = require('./lib/EngineerQues');
+const internQues = require('./lib/InternQues');
 
 // // function to initialize program
 function init() {
-    inquirer.prompt(managerQues)
+    inquirer.prompt(internQues)
     .then((data) => {
         if (!data.confirm){
-        data.employeeId = new Manager(data);
+        data.employeeId = new Intern(data);
+        console.log(data.employeeId)
+        console.log(data.employeeId.getSchool())
         console.log(data.employeeId.getRole())
         } else {
             return init()
