a2ff4b4a52c0606b39d81aedb13f22802ca4f756
ian29012 <iancheng29012@gmail.com>
2023-01-30 11:36:44 +0000

    fixed the problem with can't generate the file

---
+++ Employee_Generate.md
@@ -0,0 +1,6 @@
+
+    ian
+    ian
+    ian
+    ian
+    Engineer
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const generator = require('./lib/generator.js')
 const employeeQues = require('./lib/employee.js');
 
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generator(data), (err) =>
+    err ? console.error(err) : console.log('Success! employee was generated!'));
+  }
 
+// // function to initialize program
 function init() {
     inquirer.prompt(employeeQues)
-    .then((response) => {})
+    .then((response) => {
+        writeToFile("./Employee_Generate.md" ,response)
+    });
 }
 init()
